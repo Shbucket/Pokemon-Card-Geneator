f24f66eed265909aedf866b1cbbc3273d4389eb9
shoamri joseph <shomarijoseph@gmail.com>
2022-12-08 17:04:15 -0500

    created function to find pokemon that matches id

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cont = document.getElementById('container');
+const cont = document.getElementById("container");
 const fetchPokemon = () => {
   const promises = [];
   for (let i = 1; i < 150; i++) {
@@ -12,28 +12,50 @@ const fetchPokemon = () => {
       image: data.sprites["front_default"],
       type: data.types.map((type) => type.type.name).join(", "),
     }));
-   displayPokemon(pokemon)
+    displayPokemon(pokemon);
   });
 };
 
-const displayPokemon = (pokemon) => {
-const pokemonTitle = pokemon
-  .map(
-    (pokeman) =>
-      `<li class='card'>
+const displayPokemon = (data) => {
+  const pokemonTitle = data
+    .map(
+      (pokeman) =>
+        `<li class='card'>
   <h1 class='card-title'>${pokeman.id}. ${pokeman.name}</h1>
    <img class='card-image' src="${pokeman.image}"/>
   <p class='card-subtitle'>Type: ${pokeman.type}</p>
   
   </li>`
-  )
-  .join(" ");
+    )
+    .join(" ");
 
-  cont.innerHTML = pokemonTitle
-
-}
-
-const findPokemon = (${number.value}) {
+  cont.innerHTML = pokemonTitle;
+};
+const displayPoke = (poke) => {
+  const pokemonInfo = `<li class='card'>
+  <h1 class='card-title'>${poke.id}. ${poke.name}</h1>
+   <img class='card-image' src="${poke.image}"/>
+  <p class='card-subtitle'>Type: ${poke.type}</p>
   
+  </li>`;
+  cont.innerHTML = pokemonInfo
 }
+
+const findPokemon = () => {
+  cont.replaceChildren();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${number.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const poke = {
+        name: data.name,
+        id: data.id,
+        image: data.sprites["front_default"],
+        type: data.types.map((type) => type.type.name).join(", "),
+      };
+    console.log(poke.name)
+    displayPoke(poke);
+    });
+ 
+};
+
 fetchPokemon();
