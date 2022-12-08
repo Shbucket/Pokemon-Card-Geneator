6057be7173b556ca84d3157dfd449b69d41b1bc2
shoamri joseph <shomarijoseph@gmail.com>
2022-12-08 14:42:17 -0500

    created logic to create pokemon card

---
+++ script.js
@@ -0,0 +1,30 @@
+const cont = document.getElementById("container");
+const div = document.createElement("div");
+const title = document.createElement("h1");
+const image = document.createElement('IMG')
+const type = document.createElement('p')
+
+cont.appendChild(div);
+div.appendChild(title);
+div.appendChild(image);
+div.appendChild()
+
+const fetchPokemon = () => {
+  let num = Math.floor(Math.random() * 905);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemon = {
+        name: data.name,
+        image: data.sprites["front_default"],
+        type: data.types.map((type) => type.type.name).join(", "),
+      };
+      title.textContent = pokemon.name;
+      image.src = pokemon.image
+
+      console.log( 'hello ' + pokemon.image);
+    });
+};
+fetchPokemon();
+console.log(pokemon.name);
+
