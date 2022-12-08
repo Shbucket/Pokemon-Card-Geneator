0fe4ae1eecab8340e664c6dc3ea841e7128847f9
shoamri joseph <shomarijoseph@gmail.com>
2022-12-08 18:06:25 -0500

    added code to captialize first letter of name

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cont = document.getElementById("container");
+//fetches information to
 const fetchPokemon = () => {
   const promises = [];
   for (let i = 1; i < 152; i++) {
@@ -10,20 +11,26 @@ const fetchPokemon = () => {
       name: data.name,
       id: data.id,
       image: data.sprites["front_default"],
+      weight: data.weight,
+      height: data.height,
       type: data.types.map((type) => type.type.name).join(", "),
     }));
     displayPokemon(pokemon);
   });
 };
-
+//creates card to display pokemon details
 const displayPokemon = (data) => {
   const pokemonTitle = data
     .map(
       (pokeman) =>
         `<li class='card'>
-  <h1 class='card-title'>${pokeman.id}. ${pokeman.name}</h1>
+  <h1 class='card-title'>${pokeman.id}. ${
+          pokeman.name.charAt(0).toUpperCase() + pokeman.name.slice(1)
+        }</h1>
    <img class='card-image' src="${pokeman.image}"/>
-  <p class='card-subtitle'>Type: ${pokeman.type}</p>
+  <p class='card-subtitle'>Type: ${pokeman.type} | Height: ${
+          pokeman.height
+        } | Weight: ${pokeman.weight}</p>
   
   </li>`
     )
@@ -35,14 +42,18 @@ const displayPokemon = (data) => {
 //displays pokemon that matches user entered number from input form
 const displayPoke = (poke) => {
   const pokemonInfo = `<li class='card'>
-  <h1 class='card-title'>${poke.id}. ${poke.name}</h1>
+  <h1 class='card-title'>${poke.id}. ${
+    poke.name.charAt(0).toUpperCase() + poke.name.slice(1)
+  }</h1>
    <img class='card-image' src="${poke.image}"/>
-  <p class='card-subtitle'>Type: ${poke.type}</p>
+  <p class='card-subtitle'>Type: ${poke.type} | Height: ${
+    poke.height
+  } | Weight: ${poke.weight} |</p>
   
   </li>`;
-  cont.innerHTML = pokemonInfo
-}
-//fetches pokemon that matches user entered number from input form and replaces dom children to feed in new one 
+  cont.innerHTML = pokemonInfo;
+};
+//fetches pokemon that matches user entered string from input form and replaces dom children to feed in new one
 const findPokemon = () => {
   cont.replaceChildren();
   fetch(`https://pokeapi.co/api/v2/pokemon/${text.value.toLowerCase()}`)
@@ -53,11 +64,12 @@ const findPokemon = () => {
         id: data.id,
         image: data.sprites["front_default"],
         type: data.types.map((type) => type.type.name).join(", "),
+        weight: data.weight,
+        height: data.height,
+        moves: data.moves.map((move) => move.move.name).join(", "),
       };
-    console.log(poke.name)
-    displayPoke(poke);
-    }).catch((err)=> cont.innerHTML = 'Pokemon not found please try again');
- 
+      console.log(poke.name);
+      displayPoke(poke);
+    })
+    .catch((err) => (cont.innerHTML = "Pokemon not found please try again"));
 };
-
-fetchPokemon();
